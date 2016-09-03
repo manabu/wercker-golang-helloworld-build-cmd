ee5bc6935f7a0cd62b85a69ebf7d258c9f2734a2
Manabu ISHII <manabu.ishii.q@gmail.com>
2016-09-04 04:03:05 +0900

    Fix version line

--- helloworld.go
+++ helloworld.go
@@ -1,15 +1,15 @@
 package main
 
-const version = "0.1.0"
-
 import (
-    "fmt"
-    "io"
-    "os"
+	"fmt"
+	"io"
+	"os"
 )
 
-func testPrint(w io.Writer){
-	fmt.Fprint(w,"Hello world!!\n")
+const version = "0.1.0"
+
+func testPrint(w io.Writer) {
+	fmt.Fprint(w, "Hello world!!\n")
 }
 
 func main() {
